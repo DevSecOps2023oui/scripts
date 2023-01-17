e2053ee1c4202b842d08a81b4991628f1b78d6de
maxentr <rouaultmaxent@gmail.com>
2023-01-17 09:23:59 +0100

    feat: add front script

---
+++ continuous-integration/front.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RELATIVE_PATH="/home/ubuntu"
+REPOSITORY="${RELATIVE_PATH}/Front"
+GITHUB="https://github.com/DevSecOps2023oui/Front.git"
+
+WWW="/var/www/html"
+
+# Check if repository exists and pull, if not clone it
+if [ -d "$REPOSITORY" ]; then
+  cd $REPOSITORY || exit
+  git pull || exit
+  npm ci || npm i || exit
+else
+  cd $RELATIVE_PATH || exit
+  git clone $GITHUB || exit
+  cd $REPOSITORY || exit
+  npm i || exit
+fi
+
+# Build
+npm run build || exit
+
+# Copy files to www
+cp "$REPOSITORY/dist" $WWWW || exit
